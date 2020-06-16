7c7016d9426bcff61e3a8cc0784296638018e287
Arom Jhee <aromjhee@gmail.com>
2020-06-15 19:56:58 -0700

    create routes and pug files for CRUD on project model

--- db/models/project.js
+++ db/models/project.js
@@ -4,11 +4,31 @@ module.exports = (sequelize, DataTypes) => {
     projectName: {
       allowNull: false,
       type: DataTypes.STRING(75),
+      validate: {
+        notEmpty: {
+          msg: 'Project name cannot be empty'
+        },
+        notNull: {
+          msg: 'Project name cannot be empty'
+        },
+        len: {
+          args: [0, 75],
+          msg: 'Project name must not be less than 0 or more than 75 characters'
+        }
+      },
     },
     teamId: {
       allowNull: false,
       type: DataTypes.INTEGER,
-      references: { model: 'Teams' }
+      references: { model: 'Teams' },
+      validate: {
+        notEmpty: {
+          msg: 'Team ID cannot be empty'
+        },
+        notNull: {
+          msg: 'Team ID cannot be empty'
+        },
+      }
     },
   }, {});
   Project.associate = function(models) {
