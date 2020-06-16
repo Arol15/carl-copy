13c44a2319fd990a6f8db1b7d5d068d3f7099162
Arom Jhee <aromjhee@gmail.com>
2020-06-16 08:22:33 -0700

    update layout.pug file links

--- routes/root.js
+++ routes/root.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const { Project, Team, Column, Task } = require('../db/models');
 
 router.get('/', (req, res) => {
-  res.render('layout')
+  const projects = Project.findAll();
+  const teams = Team.findAll();
+  const columns = Column.findAll();
+  const tasks = Task.findAll();
+
+
+  res.render('layout', { projects, teams, columns, tasks });
 })
 
 module.exports = router
