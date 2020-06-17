0d3cb2e32daf8fc877ccb54e7a69dc1120e56375
Arom Jhee <aromjhee@gmail.com>
2020-06-17 10:08:32 -0700

    wip

--- routes/users.js
+++ routes/users.js
@@ -16,14 +16,13 @@ router.get('/users', asyncHandler(async (req, res) => {
 router.get('/users/register', csrfProtection, asyncHandler(async (req, res) => {
   const user = db.User.build()
   const teams = await db.Team.findAll()
-  console.log("teams", teams);
   res.render('users/user-register', { user, teams, token: req.csrfToken() })
 }))
 
 
 router.post('/users/register', csrfProtection, asyncHandler(async (req, res) => {
   const { firstName, lastName, email, password, confirmPassword, teamId } = req.body
-
+  const teams = db.Team.findAll()
   const user = db.User.build({ firstName, lastName, email, hashedPassword: password, teamId })
   // TODO: add confirm password validation
   try {
@@ -32,12 +31,12 @@ router.post('/users/register', csrfProtection, asyncHandler(async (req, res) => 
     res.redirect(`/teams/${user.teamId}/projects`)
   } catch (err) {
     const errors = err.errors.map(error => error.message)
-    // res.render('users/user-register', {
-    //   errors,
-    //   user,
-    //   token: req.csrfToken()
-    // })
-    res.redirect('/users/register')
+    res.render('users/user-register', {
+      errors,
+      user,
+      teams,
+      token: req.csrfToken()
+    })
   }
 }))
 
