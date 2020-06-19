86c609e880c1c8c471b7fb735f822b99c620bb48
Arom Jhee <aromjhee@gmail.com>
2020-06-18 20:10:30 -0700

    finish custom error handler test

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
   if (process.env.NODE_ENV === 'production') {
     // TODO Log the error to the database.
   } else {
-    console.error(err)
+    // console.error(err)
   }
   next(err)
 })
