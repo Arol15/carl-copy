0668be65705135162bf23c14512a02a709f574b5
ColeRutledge <ColeRutledge@gmail.com>
2020-06-19 18:17:49 -0400

    get drag and drop rendering without styles

--- public/src/index.js
+++ public/src/index.js
@@ -3,6 +3,55 @@ const React = window.React;
 const ReactDOM = window.ReactDOM;
 const { DragDropContext, Draggable, Droppable } = window.ReactBeautifulDnd;
 
+class Task extends React.Component {
+  render() {
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+
+        {(provided, snapshot) => (
+
+          <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} isDragging={snapshot.isDragging}>
+
+            {this.props.task.content}
+
+          </div>
+        )}
+      </Draggable>
+    )
+  }
+}
+
+class Column extends React.Component {
+  render() {
+    return (
+      <Draggable draggableId={this.props.column.id} index={this.props.index}>
+        {(provided) => (
+
+          <div {...provided.draggableProps} ref={provided.innerRef}>
+            <h1 {...provided.dragHandleProps}>{this.props.column.title}</h1>
+
+            <Droppable type='task' droppableId={this.props.column.id}>
+              {(provided, snapshot) => (
+
+                <div ref={provided.innerRef} {...provided.droppableProps} isDraggingOver={snapshot.isDraggingOver}>
+
+                  {this.props.tasks.map((task, index) => (
+                    <Task key={task.id} task={task} index={index} />
+                  ))}
+
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+
+          </div>
+
+        )}
+      </Draggable>
+    )
+  }
+}
+
 class App extends React.Component {
   state = {
     tasks: {
@@ -32,12 +81,106 @@ class App extends React.Component {
     columnOrder: ['column-1', 'column-2', 'column-3'],
   }
 
+  onDragStart = start => {
+    const homeIndex = this.state.columnOrder.indexOf(start.source.droppableId)
+    this.setState({ homeIndex })
+  }
+
+  onDragEnd = result => {
+    document.body.style.color = 'inherit'
+    document.body.style.backgroundColor = 'inherit'
+    this.setState({ homeIndex: null })
+
+    const { destination, source, draggableId, type } = result
+
+    if (!destination) { return }
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) { return }
+
+    if (type === 'column') {
+      const newColumnOrder = Array.from(this.state.columnOrder)
+      newColumnOrder.splice(source.index, 1)
+      newColumnOrder.splice(destination.index, 0, draggableId)
+
+      const newState = { ...this.state, columnOrder: newColumnOrder }
+
+      this.setState(newState)
+      return
+    }
+
+    const start = this.state.columns[source.droppableId]
+    const finish = this.state.columns[destination.droppableId]
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds)
+      newTaskIds.splice(source.index, 1)
+      newTaskIds.splice(destination.index, 0, draggableId)
+
+      const newColumn = { ...start, taskIds: newTaskIds }
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn,
+        }
+      }
+
+      this.setState(newState)
+      // TODO: persist order state to database here
+      return
+    }
+
+    // moving from one list to another
+    const startTaskIds = Array.from(start.taskIds)
+    startTaskIds.splice(source.index, 1)
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    }
+
+    const finishTaskIds = Array.from(finish.taskIds)
+    finishTaskIds.splice(destination.index, 0, draggableId)
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    }
+
+    this.setState(newState)
+    // TODO: persist order state to database here
+  }
+
   render() {
     if (this.state.liked) {
       return 'You liked this.';
     }
     return (
-      <button onClick={() => { }}>Like</button>
+      <DragDropContext onDragStart={this.onDragStart} onDragEnd={this.onDragEnd}>
+        <Droppable droppableId='all-columns' direction='horizontal' type='column'>
+          {(provided) => (
+
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+
+              {this.state.columnOrder.map((columnId, index) => {
+                const column = this.state.columns[columnId]
+                const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+                return <Column key={column.id} column={column} tasks={tasks} index={index} />
+              })}
+
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     )
   }
 }
